18a9bfe0ffee12a6e1ee8bb34f37c3dd81fbdb59
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-11-01 16:19:44 -0300

    STRUCTURE - Adicionando Conteudo Basico nos Arquivos - [DONE]

---
+++ Back-End/models/connections.js
@@ -0,0 +1,15 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const connection = () => MongoClient
+.connect(process.env.MONGO_DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then((conn) => conn.db(process.env.DB_NAME))
+.catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
+
+module.exports = connection;
---
+++ Back-End/services/valid.js
@@ -0,0 +1,4 @@
+const Joi = require('joi');
+const {} = require('../models/');
+
+module.exports = {}
