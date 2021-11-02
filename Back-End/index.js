13f0dcd70af2e3575b488fd3ea47758101304081
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-11-02 16:15:04 -0300

    STRUCTURE - Adicionando estrutura do tokenJWT & estrutura de testes de integraçao - [DONE]

--- Back-End/index.js
+++ Back-End/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 // const { taskRouter, loginRouter, userRouter } = require('./routes/index');
-
+require('dotenv').config();
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -15,6 +15,6 @@ app.get('/', (req, res) => {
 
 // app.use('/login', loginRouter);
 
-// app.use('/user', userRouter);
+// app.use('/users', userRouter);
 
 app.listen(PORT, () => console.log(`Conectado na Porta ${PORT}`));
--- Back-End/tests/test.js
+++ Back-End/tests/test.js
@@ -1,7 +1,40 @@
+const frisby = require('frisby');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
+const { DB_NAME } = process.env;
+const URL = 'http://localhost:3000';
 
-describe('', () => {
-  it('', async () => {
+describe('1 - Crie um endpoint para o cadastro de usuários', () => {
+  let connection;
+  let db;
 
+  beforeAll(async () => {
+    connection = await MongoClient.connect('mongodb://localhost:27017/Ebytr/', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    db = connection.db(DB_NAME);
+  });
+
+  beforeEach(async () => {
+    await db.collection('users').deleteMany({});
+    const users = {
+      name: 'admin', email: 'root@gmail.com', password: 'admin' 
+    };
+    await db.collection('users').insertOne(users);
+  });
+
+  afterAll(async () => {
+    await connection.close();
   })
-})
+
+  it('Verificar se houve conexão com a API ', async () => {
+    await frisby
+      .get(`${URL}`)
+      .then((res) => {
+        const { body } = res;
+        expect(body).toBe('Hello World');
+      });
+  })
+});
