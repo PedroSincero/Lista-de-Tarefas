725b262640b01f4d170ca045265e06f609c94d4c
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-11-03 15:24:17 -0300

    FEAT - Adicionando conteudo da funçao 'add' controller - [DONE]

--- Back-End/controllers/userController.js
+++ Back-End/controllers/userController.js
@@ -2,7 +2,10 @@ const userModel = require('../models/userModel');
 const {} = require('../services/userValid');
 
 const add = async (req, res) => {
+  const { name, email, password } = req.body;
 
+  const [user] = await userModel.add(name, email, password);
+  return res.status(201).json({ user });
 }
 
 module.exports = {
--- Back-End/models/userModel.js
+++ Back-End/models/userModel.js
@@ -1,7 +1,20 @@
 const connection = require('./connections');
 
-const add = async () => {
+const serialize = ({_id, name, email, password}) => {
+  return {
+    _id,
+    name,
+    email,
+    password,
+  }
+}
 
+const add = async (name, email, password) => {
+  const db = connection();
+  const addUser = await db.collection('users')
+  .insertOne({ name, email, password });
+  const result = addUser.ops;
+  return result.map(serialize);
 };
 
 const find = async () => {
