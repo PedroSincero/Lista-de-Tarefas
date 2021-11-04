aa64011c4024018aaec5c6cd1c2a0f341a8fb9a3
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-11-04 18:22:00 -0300

    FIX - Pegando Valores do back-end  e adicionando no front

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './styles/App.css';
 import * as sH from './styles/Home';
 import { ListItem } from './components/ListItem';
 import { AddArea } from './components/AddArea';
+import api from './services/api';
 
 function App() {
+  const [task, setTask] = useState();
+
+  useEffect(() => {
+    api
+      .get('/tasks')
+      .then((response) => setTask(response.data))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
+
   return (
     <sH.Container>
 
       <sH.Area>
         <sH.Header> Lista de Tarefas </sH.Header>
       <AddArea />
-      <ListItem />
+      {task && task.sucess.map(({ _id, task, status }, index) => (
+        <ListItem key={index} _id={_id} task={task} status={status}/>
+      ))}
       </sH.Area>
 
     </sH.Container>
--- FrontEnd/src/components/ListItem/index.js
+++ FrontEnd/src/components/ListItem/index.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import * as S from './styles';
 import { MdDeleteOutline }  from 'react-icons/md';
 import { AiOutlineEdit } from 'react-icons/ai';
+import PropTypes from 'prop-types';
 
-export const ListItem = () => {
-  const [statusList, setStatusList] = useState('Pendente');
+export const ListItem = ({_id, task, status }) => {
+
+  const [setStatusList] = useState('Pendente');
+
 
   const addStatus = (value) => {
-    console.log(value);
     if(value === 'Pendente')  return setStatusList('Em andamento');
     if(value === 'Em andamento') return setStatusList('Pronto');
     if(value === 'Pronto') return setStatusList('Pendente');
@@ -15,12 +17,18 @@ export const ListItem = () => {
 
   return (
     <S.Container>
-    <button className="delete" type="button" onClick={() => console.log('deletando')}><MdDeleteOutline /></button>
+    <button id={_id} className="delete" type="button" onClick={() => console.log('deletando')}><MdDeleteOutline /></button>
     <button className="edit" type="button" onClick={() => console.log('editando')}><AiOutlineEdit /></button>
-      task teste
-    <button className="status" type="button"  onClick={(e) => addStatus(e.target.textContent)}>{statusList}</button>
+      {task}
+    <button id={_id}className="status" type="button"  onClick={(e) => addStatus(e.target.textContent)}>{status}</button>
     </S.Container>
   )
 }
+
+ListItem.propTypes = {
+  _id: PropTypes.string.isRequired,
+  task: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+};
 
 // Agradecimentos a Leandro Reis por me auxiliar na utilização do React Icons *
