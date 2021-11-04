d68de1766a729789502c38b413ca33dc1553dbca
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-11-04 20:57:00 -0300

    Adicionando css da ediçao

--- FrontEnd/src/components/ListItem/index.js
+++ FrontEnd/src/components/ListItem/index.js
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './styles';
 import { MdDeleteOutline }  from 'react-icons/md';
 import { AiOutlineEdit } from 'react-icons/ai';
 import PropTypes from 'prop-types';
 
 export const ListItem = ({_id, task, status, handleUpdate }) => {
-
+  const [taskEdit, setTaskEdit] = useState(false);
   const addStatus = async (value) => {
     if(value === 'Pendente') return await handleUpdate(_id, task, 'Em Andamento'); 
     if(value === 'Em Andamento') return await handleUpdate(_id, task, 'Pronto');
     if(value === 'Pronto') return await handleUpdate(_id, task, 'Pendente');
   }
 
+  // useEffect(() => {
+
+  // }, [taskEdit]);
+
+  const editTask = (value) => {
+    if(value === false) {
+      setTaskEdit(true);
+    }
+    if(value === true) {
+      setTaskEdit(false);
+    }
+
+  }
+
   return (
+    <>
     <S.Container>
     <button id={_id} className="delete" type="button" onClick={() => console.log('deletando')}><MdDeleteOutline /></button>
-    <button className="edit" type="button" onClick={() => console.log('editando')}><AiOutlineEdit /></button>
+    <button className="edit" type="button" onClick={() => editTask(taskEdit)}><AiOutlineEdit /></button>
       {task}
     <button id={_id} className="status" type="button"  onClick={(e) => addStatus(e.target.textContent)}>{status}</button>
     </S.Container>
+    {taskEdit ? (
+      <S.Input
+        type="text"
+        placeholder="Edite a Tarefa"
+      />) : undefined}
+    </>
   )
 }
 
--- FrontEnd/src/components/ListItem/styles.js
+++ FrontEnd/src/components/ListItem/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.div`
   display: flex;
   background-color: #20212C;
-  padding: 10px;
+  padding: 15px;
   border-radius: 10px;
   margin-bottom: 10px;
   align-items: center;
@@ -26,3 +26,12 @@ export const Container = styled.div`
     padding: 4px;
   }
 `;
+
+export const Input = styled.input`
+  background-color: transparent;
+  border-radius: 15px;
+  border: 1px solid #555;
+  padding: 10px;
+  color: #fff;
+  font-size: 18px;
+`
