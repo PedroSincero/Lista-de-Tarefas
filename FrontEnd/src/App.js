f6907c8f0e0bb633cd2df25196928d4ad2d04b71
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-11-04 13:44:13 -0300

    FIX - Adicionando Styled Component & conteudo basico no App

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/App.css';
+import * as sH from './styles/styleHome';
+
+function App() {
+  return (
+    <sH.Container>
+      <sH.Area>
+        <sH.Header> Lista de Tarefas </sH.Header>
+      </sH.Area>
+    </sH.Container>
+  );
+}
+
+export default App;
---
+++ FrontEnd/src/styles/styleHome.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+
+// Teste
+// export const Title = styled.h1`
+//   color: #f00;
+//   font-size: ${props => `${props.fontSize}px`};
+//   span {
+//     color: black;
+//     font-size: 12px;
+//   }
+// `;
+// // Pegando as info de Title e adicionando novos valores
+// export const TitleSmall = styled(Title)`
+//   color: #00f;
+//   font-size: 16px;
+// `;
+
+export const Container = styled.div`
+  background-color: black;
+  color: white;
+  min-height: 100vh;
+`;
+
+export const Area = styled.div`
+  margin: auto;
+  max-width: 980px;
+  padding: 10px;
+`;
+
+export const Header = styled.h1`
+  color: #fff; 
+  text-align: center;
+  border-bottom: 1px solid #444;
+  padding-bottom: 20px;
+  font-size: 40px;
+`;
+
+// Agradecimentos a https://www.youtube.com/watch?v=R3S8DEzEn6s este video me auxiliou na utilização do styledComponents
