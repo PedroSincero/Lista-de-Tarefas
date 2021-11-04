ca3ed6ec39253527cab6a48a6e385469cdc1e779
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-11-04 15:29:49 -0300

    FIX - Finalizando estruturas do Front end && CSS - [DONE]

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import './styles/App.css';
 import * as sH from './styles/Home';
 import { ListItem } from './components/ListItem';
+import { AddArea } from './components/AddArea';
 
 function App() {
   return (
     <sH.Container>
+
       <sH.Area>
         <sH.Header> Lista de Tarefas </sH.Header>
-      </sH.Area>
-      
+      <AddArea />
       <ListItem />
+      </sH.Area>
+
     </sH.Container>
   );
 }
