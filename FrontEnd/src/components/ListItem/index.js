ebcec342776eb84734567079bb33cceea9fc3c28
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-11-04 17:18:25 -0300

    Fix - Adicionando Logica de Status - [DONE]

--- FrontEnd/src/components/ListItem/index.js
+++ FrontEnd/src/components/ListItem/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './styles';
 import { MdDeleteOutline }  from 'react-icons/md';
 import { AiOutlineEdit } from 'react-icons/ai';
 
 export const ListItem = () => {
+  const [statusList, setStatusList] = useState('Pendente');
+
+  const addStatus = (value) => {
+    console.log(value);
+    if(value === 'Pendente')  return setStatusList('Em andamento');
+    if(value === 'Em andamento') return setStatusList('Pronto');
+    if(value === 'Pronto') return setStatusList('Pendente');
+  }
+
   return (
     <S.Container>
     <button className="delete" type="button" onClick={() => console.log('deletando')}><MdDeleteOutline /></button>
     <button className="edit" type="button" onClick={() => console.log('editando')}><AiOutlineEdit /></button>
       task teste
-    <button className="status" onClick={() => console.log('alterando status')}>Pendente</button>
+    <button className="status" type="button"  onClick={(e) => addStatus(e.target.textContent)}>{statusList}</button>
     </S.Container>
   )
 }
 
-// Agradecimentos a Leandro Reis por me auxiliar na utilização do React Icons
+// Agradecimentos a Leandro Reis por me auxiliar na utilização do React Icons *
