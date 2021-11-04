d26f8924ecbf7d85c5fe422c001b364d8c8c92c2
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-11-04 02:32:22 -0300

    TEST - Adicionando test do metodo DELETE da rota '/tasks' - [DONE]

--- Back-End/tests/task.test.js
+++ Back-End/tests/task.test.js
@@ -67,7 +67,7 @@ describe('3 - Validando Metodo POST da Rota "/tasks"  ', () => {
   })
 });
 
-describe('4 - Validando Metodo GET da Rota "/tasks"  ', () => {
+describe('4 - Validando Metodo GET da Rota "/tasks"', () => {
   let connection;
   let db;
 
@@ -231,4 +231,74 @@ describe('5 - Validando Metodo PUT da rota "/tasks"',  () => {
         expect(result).toHaveProperty('sucess');
       })
   })
-})
+});
+
+describe.only('6 - Validando Metodo DELETE da rota "/tasks"', () => {
+  let connection;
+  let db;
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect('mongodb://localhost:27017/Ebytr/', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    db = connection.db(DB_NAME);
+  });
+
+  beforeEach(async () => {
+    await db.collection('tasks').deleteMany({});
+    const task = {
+      task: 'admin'
+    };
+    await db.collection('tasks').insertOne(task);
+  });
+
+  afterAll(async () => {
+    await connection.close();
+  });
+
+  it('Será Validado se houve conexão com a API ', async () => {
+    await frisby
+      .get(`${URL}`)
+      .then((res) => {
+        const { body } = res;
+        expect(body).toBe('Hello World');
+      });
+  });
+
+  it('Será Validado se o campo "id" é invalido', async () => {
+    await frisby
+    .delete(`${URL}/tasks`,
+    {
+      id: '0010001000100',
+    })
+    .expect('status', 400)
+    .then((res) => {
+      const { body } = res;
+      const result = JSON.parse(body);
+      expect(result.message).toBe('Invalid ID, try again.');
+    })
+  });
+
+  it('Será validade se a task é excluida com sucesso', async () => {
+    let resultDelete;
+    
+    await frisby
+    .post(`${URL}/tasks`,
+    {
+      task: 'testeTASK'
+    })
+    .expect('status', 201)
+    .then((res) => {
+      const { body } = res;
+      resultDelete = JSON.parse(body);
+    });
+
+    await frisby
+      .delete(`${URL}/tasks`,
+      {
+        id: resultDelete.sucess
+      })
+      .expect('status', 204)
+  });
+});
